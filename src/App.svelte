dedd0935ef6636b843a4c3193e7bf03fe2e9e14f
Strichmanni <mschuck.cb@gmail.com>
2025-11-29 12:39:06 +0100

    Add App.svelte with boot screen logic

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import BootScreen from './BootScreen.svelte';
+  import Desktop from './Desktop.svelte';
+  import { writable } from 'svelte/store';
+  let bootComplete = writable(false);
+
+  function handleBootComplete() {
+    bootComplete.set(true);
+  }
+</script>
+
+{#if !$bootComplete}
+  <BootScreen onBootComplete={handleBootComplete}/>
+{:else}
+  <Desktop/>
+{/if}
